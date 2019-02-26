f28cde653dde59489340ee438decd3410ed6d3ac
Ben Parisot <me@benparisot.com>
2019-02-25 17:08:43 -0800

    style header and footer

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import makeHTMLTemplate from './src/create-image-list.js';
 import makeHeaderTemplate from './src/create-header.js';
 import images from './data/images.js';
+import makeFooter from './src/create-footer.js';
 
 const headerNode = document.getElementById('head');
 const imageListNode = document.getElementById('image-list');
+const footerNode = document.getElementById('foot');
 
 const headerDOM = makeHeaderTemplate();
 headerNode.appendChild(headerDOM);
@@ -12,3 +14,7 @@ images.forEach(item => {
     const dom = makeHTMLTemplate(item);
     imageListNode.appendChild(dom);
 });
+
+console.log(footerNode);
+const footerDOM = makeFooter();
+footerNode.appendChild(footerDOM);
